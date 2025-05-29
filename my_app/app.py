8312134ff738ac35d5ed750b9288acf2563f0583
admin7 <admin@istra-terminal.ru>
2025-05-29 19:47:39 +0300

    New Function + New Tests

--- my_app/app.py
+++ my_app/app.py
@@ -1,2 +1,5 @@
 def add(a, b):
     return a + b
+
+def add(c, d ,u):
+    return (c+d)*u
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,8 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(5, 5) == 10
+
+def test2_add():
+    assert add(1, 1, 1) == 2
+    assert add(2, 2 ,2) == 8
+    assert add(-1, 0, 5) == -5
