fd8d421dc77879e74af3d942ad752cefed58df9b
admin7 <admin@istra-terminal.ru>
2025-05-30 10:16:14 +0300

    New

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,9 +5,9 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(5, 5) == 10
-    
 
-def test2_add():
-    assert add(1, 1, 1) == 2
-    assert add(2, 2, 2) == 8
-    assert add(-1, 0, 5) == -5
+
+def test_add_new_func():
+    assert add_new_func(1, 1, 1) == 2
+    assert add_new_func(2, 2, 2) == 8
+    assert add_new_func(-1, 0, 5) == -5
