0521379ce3e4f1f5c31b59df03c65a462dfdb002
admin7 <admin@istra-terminal.ru>
2025-06-03 13:42:03 +0300

    New code + tests for it

--- my_app/parser.py
+++ my_app/parser.py
@@ -17,3 +17,11 @@ def max_person(data):
 
 def min_person(data):
     return min(data, key=data.get)
+
+
+def add(a, b):
+    return a + b
+
+
+def add_new_func(c, d, u):
+    return (c + d) * u
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,5 @@
 from my_app import parser
+from my_app.app import add, add_new_func
 
 
 def test_parse_input():
@@ -20,3 +21,15 @@ def test_max_person():
 def test_min_person():
     d = {"John": 100, "Anna": 200, "Tom": 150}
     assert parser.min_person(d) == "John"
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(5, 5) == 10
+
+
+def test_add_new_func():
+    assert add_new_func(1, 1, 1) == 2
+    assert add_new_func(2, 2, 2) == 8
+    assert add_new_func(-1, 0, 5) == -5
