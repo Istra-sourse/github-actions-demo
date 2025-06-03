cc63b3c382cefaa19de1c04b27ae5c38098cc0e3
admin7 <admin@istra-terminal.ru>
2025-06-03 14:29:23 +0300

    Added auto PH + new Tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -34,3 +34,4 @@ def test_add_new_func():
     assert add_new_func(1, 1, 1) == 2
     assert add_new_func(2, 2, 2) == 8
     assert add_new_func(-1, 0, 5) == -5
+    assert add_new_func(0, 0, 0) == 0
